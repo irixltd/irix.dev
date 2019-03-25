597ca3d59265e88eab3ecf5ad5bd558ca2aad835
mattcroberts <mattroberts.uk@gmail.com>
2019-03-25 20:58:55 +0000

    sleep before curl test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,6 +8,8 @@ echo "TAG=$TAG"
 
 docker-compose -f docker-compose.yml -f ./tictactoe/docker-compose.prod.yml up -d
 
+sleep 5
+
 docker ps -a
 
 curl --fail https://irix.dev/ && curl --fail https://irix.dev/tictactoe
