63addd1bba667415e0c34aa9fad5b3d7aa0d7dee
mattcroberts <mattroberts.uk@gmail.com>
2019-04-09 00:35:06 +0100

    fix nginx restarts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,9 +10,6 @@ docker-compose -f docker-compose.yml -f ./tictactoe/docker-compose.prod.yml up -
 
 sleep 5
 
-docker-compose -f docker-compose.yml -f ./tictactoe/docker-compose.prod.yml exec nginx nginx -t
-docker-compose -f docker-compose.yml -f ./tictactoe/docker-compose.prod.yml exec nginx nginx -s reload
-
 docker ps -a
 
 curl --fail https://irix.dev/ && curl --fail https://irix.dev/tictactoe
