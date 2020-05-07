2e1f4ba1d566d4318ce5ce4633db50ddb1a135b5
Matt Roberts <mattroberts.uk@gmail.com>
2020-05-07 12:27:09 +0100

    docker login for github package registry

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,11 +6,15 @@ source "${BASH_SOURCE%/*}/env.sh"
 
 echo "TAG=$TAG"
 
+echo $GITHUB_PERSONAL_TOKEN | docker login https://docker.pkg.github.com -u mattcroberts --password-stdin
+
 docker-compose -f docker-compose.yml \
     -f ./tictactoe/docker-compose.prod.yml \
-    -f ./rightmove-commute/docker-compose.yml
-    -f ./metrix/docker-compose.yml
-    \ up -d
+    -f ./rightmove-commute/docker-compose.yml \
+    -f ./metrix/docker-compose.yml \
+    up \
+    -d \
+    --remove-orphans
 
 sleep 5
 
